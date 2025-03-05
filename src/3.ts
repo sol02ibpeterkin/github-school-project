d77607d8a82296b1b1b0f69c45acd96f08b9514c
sol02ibpeterkin <sol02ibpeterkin@gmx.com>
2025-03-05 14:40:45 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { environment } from '../environments/environment';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private geolocation: Geolocation
+  ) {
+    this.initializeApp();
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
